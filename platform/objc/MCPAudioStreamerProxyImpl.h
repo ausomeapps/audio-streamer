41c5b8ad9776b47ec9948c34fc75ff4a17a7a597
Syed Haris Ali <syedhali07@gmail.com>
2017-02-09 14:40:57 -0600

    Cleaned up platform specific implementations of logs and added comments

--- platform/objc/MCPAudioStreamerProxyImpl.h
+++ platform/objc/MCPAudioStreamerProxyImpl.h
@@ -1,8 +1,16 @@
 #import "MCPAudioStreamerProxy.h"
 #import <AVFoundation/AVFoundation.h>
 
+//
+// The MCPAudioStreamerProxyImpl implements the MCPAudioStreamerProxy protocol
+// using an internal AVPlayer.
+//
 @interface MCPAudioStreamerProxyImpl : NSObject <MCPAudioStreamerProxy>
 
+//
+// An class method to provide an instance of the MCPAudioStreamerProxyImpl
+// class.
+//
 + (instancetype)proxy;
 
 @end
